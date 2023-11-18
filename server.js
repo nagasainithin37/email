72b9ad6fb2aa6e620da3158a204545a5e0116643
nagasainithin37 <nagasainithin37@gmail.com>
2023-11-18 10:45:36 +0530

    first

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const axios = require("axios");
 const expressAsyncHandler = require("express-async-handler");
 const nodemailer = require("nodemailer");
 
-
 app.use(exp.json());
 
 const transpoter = nodemailer.createTransport({
@@ -15,25 +14,33 @@ const transpoter = nodemailer.createTransport({
   },
 });
 
+app.get(
+  "/test",
+  expressAsyncHandler(async (req, res) => {
+    res.send("success");
+  })
+);
 
-app.post("/sendMail", expressAsyncHandler(async(req,res)=>{
-      const options = {
-        from: "scoretrack@outlook.com",
-        to: req.body.email,
-        subject: req.body.subject,
-        text: req.body.text,
-      };
+app.post(
+  "/sendMail",
+  expressAsyncHandler(async (req, res) => {
+    const options = {
+      from: "scoretrack@outlook.com",
+      to: req.body.email,
+      subject: req.body.subject,
+      text: req.body.text,
+    };
 
-        transpoter.sendMail(options, async function (err, info) {
-          if (err) {
-            console.log(err);
-            res.send("error");
-          } else {
-            res.send("success");
-          }
-        });
-}));
-
+    transpoter.sendMail(options, async function (err, info) {
+      if (err) {
+        console.log(err);
+        res.send("error");
+      } else {
+        res.send("success");
+      }
+    });
+  })
+);
 
 app.use((req, res, next) => {
   console.log(req.url);
